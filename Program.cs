7df320cf9e520e3c81413a234b4d3282785eb912
misha-solntcev <misha.solntcev@gmail.com>
2022-12-08 08:10:26 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 12. В одномерном массиве целых чисел a1, а2, ..., an 
+    найти номер первого четного числа. 
+    Если четных чисел нет, то ответом может быть число 0. */
+
+namespace Lab_4_1_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-10, 10);
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+
+            int num = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    num = i;
+                    break;
+                }
+            }
+            Console.WriteLine(num);
+            Console.ReadKey();
+        }
+    }
+}
